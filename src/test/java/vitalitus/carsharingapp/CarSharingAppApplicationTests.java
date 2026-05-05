a57809d429192e9470d82dac6dc25f24e0abcc10
S0rbex <vitalikmartunushun@gmail.com>
2026-05-05 21:50:17 +0300

    Initial commit

---
+++ src/test/java/vitalitus/carsharingapp/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package vitalitus.carsharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
