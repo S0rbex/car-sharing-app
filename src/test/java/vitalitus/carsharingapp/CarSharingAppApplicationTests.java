acbf72edadb7f2df7174a31183b75e7f32176d66
S0rbex <vitalikmartunushun@gmail.com>
2026-05-11 20:21:46 +0300

    add h2 db

--- src/test/java/vitalitus/carsharingapp/CarSharingAppApplicationTests.java
+++ src/test/java/vitalitus/carsharingapp/CarSharingAppApplicationTests.java
@@ -2,8 +2,10 @@ package vitalitus.carsharingapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CarSharingAppApplicationTests {
 
     @Test
